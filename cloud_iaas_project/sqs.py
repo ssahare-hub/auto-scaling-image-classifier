88aa98dd24d0ab80670b138c2e6f55da1852ea57
saisrinivas789 <saisrinivas1993@gmail.com>
2021-03-04 02:32:26 -0700

    get image name from sqs request

---
+++ cloud_iaas_project/sqs.py
@@ -0,0 +1,29 @@
+import boto3
+
+import boto3
+
+# Create SQS client
+sqs = boto3.client('sqs')
+
+
+# Get URL for SQS queue
+response = sqs.get_queue_url(QueueName='SQS_QUEUE_NAME')
+
+
+# Delete SQS queue
+#sqs.delete_queue(QueueUrl='SQS_QUEUE_NAME')
+
+queue_url = response['QueueUrl']
+print(queue_url)
+
+# Send message to SQS queue
+
+
+response1 = sqs.receive_message(QueueUrl=queue_url)
+print(response1)
+
+message = response1['Messages'][0]
+print(message)
+receipt_handle = message['Body']
+print(receipt_handle)
+
