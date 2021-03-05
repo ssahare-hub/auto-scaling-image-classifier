7761dba3afde286c1a0c417a55f4fc4f8f4689d2
Soham <sohamsahare@gmail.com>
2021-03-05 15:51:51 -0700

    Added more stuff in constants.py

--- cloud_iaas_project/constants.py
+++ cloud_iaas_project/constants.py
@@ -8,5 +8,12 @@ BUCKET_NAME = 'sss_cloud_bucket'
 
 APP_TIER_PREFIX = 'sss_app_tier_'
 
+KEY_NAME = 'aws-kp-1'
+
+SECURITY_GROUP_ID = 'sg-0c3a65f539fa2240a'
+
+# replace this with custom ami with app tier logic!!
+AMI_IMAGE_ID = 'ami-0ee8cf7b8a34448a6'
+
 MIN_APP_TIERS = 0
 MAX_APP_TIERS = 19
