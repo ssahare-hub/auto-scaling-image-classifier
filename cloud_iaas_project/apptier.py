2806e8fd138ac518c13294ce5e36e51b747b7145
saisrinivas789 <saisrinivas1993@gmail.com>
2021-03-04 19:00:23 -0700

    doing something

--- cloud_iaas_project/apptier.py
+++ cloud_iaas_project/apptier.py
@@ -12,14 +12,16 @@ while(True):
 # fetch job image from S3 using job id (image name)
     s3 = boto3.resource('s3' )
     image_tobe_processed = read_from_bucket('cc-asu-project', job_id)
-    os.system("aws s3 cp s3://cc-asu-project/"+str(image_tobe_processed)+" 1")
-    os.system("python3 image_classification.py 1 > result.txt")
 
 # process image using local model
+    os.system("aws s3 cp s3://cc-asu-project/" + str(image_tobe_processed) + " 1")
+    delete_from_bucket(bucket_name, object_name)
+    out = open("{fname}.txt".format(fname = image_tobe_processed), "w")
+    os.system("python3 image_classification.py 1 > {fname}.txt".format(fname = image_tobe_processed))
+    os.system("rm -rf 1")
 
-    python3 image_classification.py path_to_the_image
-
-# store result in s3  
+# store result in s3
+    os.system("aws s3 cp {fname}.txt s3://cc-project-results/{fname}.txt".format(fname = image_tobe_processed))
 # send message in response queue
 # check queue for any pending requests, if yes repeat
 # if queue empty, terminate
