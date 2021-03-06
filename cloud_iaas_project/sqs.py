7ee56626657788ac7929033239c66c297ab1c4e6
saisrinivas789 <saisrinivas1993@gmail.com>
2021-03-06 01:34:38 -0700

    adding imagefiles

--- cloud_iaas_project/sqs.py
+++ cloud_iaas_project/sqs.py
@@ -10,6 +10,27 @@ sqs = boto3.client('sqs')
 response = sqs.get_queue_url(QueueName='SQS_QUEUE_NAME')
 
 
+fifoqueue = sqs.create_queue(QueueName='test.fifo', Attributes={'FifoQueue': 'true'})
+
+print(fifoqueue['QueueUrl'])
+
+fifourl = fifoqueue['QueueUrl']
+
+
+'''fiforesponse = sqs.send_message(
+    QueueUrl=fifourl,MessageGroupId='3', MessageDeduplicationId='string',
+    MessageBody=('Image3'))'''
+
+fiforesponse = sqs.send_message(
+    QueueUrl=fifourl,MessageGroupId='4', MessageDeduplicationId='string',
+    MessageBody=('Image4'))
+
+response2 = sqs.receive_message(QueueUrl=fifourl)
+print(response2)
+message = response2['Messages'][0]
+print(message['Body'])
+print(message['ReceiptHandle'])
+
 # Delete SQS queue
 #sqs.delete_queue(QueueUrl='SQS_QUEUE_NAME')
 
@@ -26,17 +47,17 @@ queue_url = response['QueueUrl']
 
 #print(response1['MessageId'])
 
-response2 = sqs.receive_message(QueueUrl=queue_url)
+#response2 = sqs.receive_message(QueueUrl=queue_url)
 
-print (response2)
-print(len(response2))
+#print (response2)
+#print(len(response2))
 
-message = response2['Messages'][0]
-print (message)
+#message = response2['Messages'][0]
+#print (message)
 
-receipt_handle = message['Body']
+#receipt_handle = message['Body']
 
-print(receipt_handle)
+#print(receipt_handle)
 
 '''sqs.delete_message(
     QueueUrl=queue_url,
