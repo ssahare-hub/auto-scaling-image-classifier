d94985fbc1939f8ecfe69430627d3807c0e2a8bd
Soham <sohamsahare@gmail.com>
2021-03-07 14:58:13 -0700

    Feature | Corrections * Added helper functions * Added new constants

--- cloud_iaas_project/helper.py
+++ cloud_iaas_project/helper.py
@@ -2,131 +2,141 @@ import logging
 import boto3
 from botocore.exceptions import ClientError
 import botocore
-
 # functions for AWS ->
+
+# resource and client ojects for eacg
+sqs_res = boto3.resource('sqs')
+sqs_client = boto3.client('sqs')
+
+s3_client = boto3.client('s3')
+s3_res = boto3.resource('s3')
+
+ec2_client = boto3.client('ec2')
+ec2_res = boto3.resource('ec2')
+
+
 # SQS
 # create queue (if it doesn't exist)
+
 def create_queue(queue_name, attributes):
-    # should return queue url!!!
-    sqs = boto3.resource('sqs')
     SQS_QUEUE_NAME = queue_name
     # Create a SQS queue
-    response = sqs.create_queue(QueueName=SQS_QUEUE_NAME,Attributes=attributes)
+    response = sqs_client.create_queue(
+        QueueName=SQS_QUEUE_NAME, Attributes=attributes)
     return response.url
 
-def get_queue_attributes(queue_url, attribute_names):
-    # returns queue attributes
-    pass
+
+def get_queue_url(queue_name):
+    return sqs_client.get_queue_url(QueueName=queue_name)
+
+
+def get_queue_attributes(queue_url):
+    # for visible and invisible -> attribute_names = ['All']
+    queue_attrs = sqs_client.get_queue_attributes(
+        QueueUrl=queue_url, AttributeNames=['All'])
+    if 'Attributes' in queue_attrs:
+        return queue_attrs['Attributes']
+    else:
+        return {}
+
+def get_one_queue_attribute(queue_url, attribute_name='ApproximateNumberOfMessages'):
+    attrs = get_queue_attributes(queue_url)
+    if attribute_name in attrs:
+        return attrs[attribute_name]
+    else:
+        return '-'
+
 
 # send message to queue
 def send_message(queue_url, message_attributes, message_body):
-    sqs = boto3.client('sqs')
-    response = sqs.send_message(
+    response = sqs_client.send_message(
         QueueUrl=queue_url,
         MessageAttributes=message_attributes,
         MessageBody=message_body
     )
     return response['MessageId']
-    
+
 
 # recieve message from queue (wait time as a variable)
-def receive_message(queue_name):
-    sqs=boto3.resource('sqs')
-    queue = sqs.get_queue_by_name(QueueName=queue_name)
-    qurl= queue.url
-    print("Printing queue ur", qurl)
-    for message in queue.receive_messages():
-        print('{0}'.format(message.body))
-        
+def receive_message(queue_url, num_messages):
+    return sqs_client.receive_message(
+        QueueUrl=queue_url,
+        AttributeNames=['All'],
+        MessageAttributeNames=['All'],
+        MaxNumberOfMessages=num_messages,
+    )
+
+
+def delete_message(queue_url, receipt_handle):
+    sqs_client.delete_message(
+        QueueUrl=queue_url,
+        ReceiptHandle=receipt_handle
+    )
+
 # delete queue??
 def delete_queue(queue_url):
     pass
 
 
 # S3
+
 # create bucket (if it doesn't exist)
 def create_bucket(bucket_name, region=None):
-    # return success status
-    s3 = boto3.client('s3')
     try:
         if region is None:
-            s3.create_bucket(Bucket=bucket_name)
+            s3_client.create_bucket(Bucket=bucket_name)
             print("bucket created", bucket_name)
         else:
             location = {'LocationConstraint': region}
-            s3.create_bucket(Bucket=bucket_name,
+            s3_client.create_bucket(Bucket=bucket_name,
                              CreateBucketConfiguration=location)
     except ClientError as e:
         logging.error(e)
+    # return success status
         return False
     return True
 
 
 # add to bucket
 def upload_file(file_name, bucket_name, object_name):
-    # return success status
-    s3 = boto3.client('s3')
     # If S3 object_name was not specified, use file_name
     if object_name is None:
         object_name = file_name
-
     # Upload the file
     try:
-        response = s3.upload_file(file_name, bucket_name, object_name)
+        response = s3_client.upload_file(file_name, bucket_name, object_name)
         print(response)
     except ClientError as e:
         logging.error(e)
+    # return success status
         return False
     return True
 
 # read from bucket
 def read_from_bucket(bucket_name, object_name, expiration=7200):
-    # return file from bucket
-    # checkk if bucket exists
-    s3 = boto3.resource('s3')
-    # s3_connection = boto.connect_s3()
-    # bucket = s3_connection.get_bucket(bucket_name, validate=False)
-    # exists = s3_connection.lookup(bucket_name)
-    # obj=s3.Object(bucket_name,itemname)
-    # b=s3.Bucket(bucket_name)
-    # obj = s3.Object(bucket_name, object_name)
-    # body = obj.get()['Body'].read()
-    # # for obj in b.objects.all():
-    # #     key=obj.key
-    # #     body=obj.get()['Body'].read()
-    # print(obj)
-    # print(body)
-    # with open('myfile.png', 'w') as f:
-    #     f.write(body)
-    # s3_client = boto3.client('s3')
-    bucket=s3.Bucket(bucket_name)
-    object=bucket.Object(object_name)
-    # object.download_file(object_name)
+    # check if bucket exists
+    bucket = s3_res.Bucket(bucket_name)
+    object = bucket.Object(object_name)
     print(object)
+    # downloading file to local from bucket
+    object.download_file(object_name)
 
-
-# delete from bucket
+# delete from bucket??
 def delete_from_bucket(bucket_name, object_name):
     pass
 
 # delete bucket??
 def delete_bucket(bucket_name):
     pass
-    # s3=boto.connect_s3()
-    # bucket=s3.Bucket(bucket_name)
-    # for key in bucket.objects.all():
-    #     key.delete()
-    # bucket.delete()
 
 # EC2
 # create instances (ami code)
-def create_instance(key_name, sec_group_ids, image_id = 'ami-0ee8cf7b8a34448a6', instance_type = 't2.micro', min_count = 1, max_count = 1):
-    ec2 = boto3.client('ec2')
-    ec2_res = boto3.resource('ec2')
-    instances = ec2_res.create_instances(ImageId=image_id,MinCount=min_count, MaxCount=max_count, InstanceType=instance_type,KeyName=key_name,SecurityGroupIds=sec_group_ids)
+def create_instance(key_name, sec_group_ids, image_id='ami-0ee8cf7b8a34448a6', instance_type='t2.micro', min_count=1, max_count=1):
+    instances = ec2_res.create_instances(ImageId=image_id, MinCount=min_count, MaxCount=max_count,
+                                         InstanceType=instance_type, KeyName=key_name, SecurityGroupIds=sec_group_ids)
     print(instances.instance_type, instances.public_ip_address)
-    
-    
+
+
 # start/stop instances ??
 def interact_with_instance(instance_id, action):
     # action can be 'start' or 'stop'
@@ -134,7 +144,6 @@ def interact_with_instance(instance_id, action):
 
 # terminate instance? (self)
 def terminate_instance(instance_id):
-    ec2 = boto3.resource('ec2')
-    ids=[]
+    ids = []
     ids.append(instance_id)
-    ec2.instances.filter(InstanceIds=ids).terminate()
+    ec2_res.instances.filter(InstanceIds=ids).terminate()
