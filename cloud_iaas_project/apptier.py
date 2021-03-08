4eee226f846d01a99efbeef5ce95f499ed572fea
Soham <sohamsahare@gmail.com>
2021-03-07 23:35:53 -0700

    Fix | Send_message fix in apptier

--- cloud_iaas_project/apptier.py
+++ cloud_iaas_project/apptier.py
@@ -72,7 +72,7 @@ s3_writetofile.close()
 
 upload_file(file_to_store, BUCKET_NAME, file_to_store)
 print('uploaded file to bucket')
-send_message(response_queue_url, 'All', job_id, image_id)
+send_message(response_queue_url, {}, job_id, image_id)
 print('sent message to queue')
 
 # 5) delete message from request queue
