319df3180180825f4c7b5fb2d457783a2db0fec8
saisrinivas789 <saisrinivas1993@gmail.com>
2021-03-07 18:06:03 -0700

    final edit app-tier

--- cloud_iaas_project/apptier.py
+++ cloud_iaas_project/apptier.py
@@ -27,15 +27,15 @@ def image_classification(job_id):
 # end of function
 
 #GET REQUEST & RESPONSE QUEUE URLS
-job_url = get_queue_url(REQUEST_QUEUE_NAME)
+imageid_url = get_queue_url(REQUEST_QUEUE_NAME)
 response_queue_url = get_queue_url(RESPONSE_QUEUE_NAME)
 
 
 # GET COUNT OF VISIBLE & INVISIBLE MESSAGES
 
 def queue_length():
-    noof_visible_messages = get_one_queue_attribute(job_url, attribute_name= VISIBLE_MESSAGES)
-    noof_invisible_messages = get_one_queue_attribute(job_url, attribute_name= INVISIBLE_MESSAGES)
+    noof_visible_messages = get_one_queue_attribute(imageid_url, attribute_name= VISIBLE_MESSAGES)
+    noof_invisible_messages = get_one_queue_attribute(imageid_url, attribute_name= INVISIBLE_MESSAGES)
     length = int(noof_visible_messages) + int(noof_invisible_messages)
     return (length)
 
@@ -44,26 +44,29 @@ queue_len = queue_length()
 
 while queue_len > 0:
 # 1) receive message from request queue
-    imageid_from_request_queue_url = receive_message(job_url, 1)
+    imageid_from_request_queue_url = receive_message(imageid_url, 1)
     message = imageid_from_request_queue_url['Messages'][0]
-    job_id = message['Body']
+    body_string = message['Body']
+    body = json.loads(body_string)
+    image_id = body['task_id']
+    job_id = body['job_id']
+
 
 # 2) get image from s3
-    read_from_bucket(BUCKET_NAME, job_id)
+    read_from_bucket(BUCKET_NAME, image_id)
 
 # 3) process image and return result
-    image_classification_output = image_classification(job_id)
-    response_queue_message = job_id +" == "+image_classification_output
+    image_classification_output = image_classification(image_id)
+    response_queue_message = image_id +" == "+image_classification_output
 
 # 4) store result in s3 and put in response queue
-    file_to_store = job_id + ".txt"
+    file_to_store = image_id + ".txt"
     s3_writetofile = open(file_to_store, "w+")
     s3_writetofile.write(response_queue_message)
     s3_writetofile.close()
     upload_file(file_to_store, RESULTS_BUCKET, file_to_store)
-    send_message(response_queue_url, 'All', response_queue_message)
+    send_message(response_queue_url, 'All', job_id, image_id)
 
 # 5) delete message from request queue
     receipt_handle = message['ReceiptHandle']
     delete_message(response_queue_url, receipt_handle)
-    print('Received and deleted message: %s' % message)
