bde4ea9120d9e8a882a732eab5122a442a5b6309
saisrinivas789 <saisrinivas1993@gmail.com>
2021-03-10 20:07:34 -0700

    testing full project

--- cloud_iaas_project/constants.py
+++ cloud_iaas_project/constants.py
@@ -1,16 +1,16 @@
 # DO NOT CHANGE VALUES FOR THIS FILE
 # EVEN OUTSIDE THIS FILE !!!!
 
-REQUEST_QUEUE_NAME = 'requests_sss.fifo'
-RESPONSE_QUEUE_NAME = 'response_sss.fifo'
+REQUEST_QUEUE_NAME = 'requests_sai.fifo'
+RESPONSE_QUEUE_NAME = 'response_sai.fifo'
 
 MAX_RETRIES = 4
 
-BUCKET_NAME = 'sss-cloud-bucket'
+BUCKET_NAME = 'sai-cloud-bucket'
 
 APP_TIER_PREFIX = 'sss_app_tier_'
 
-KEY_NAME = 'aws-kp-1'
+KEY_NAME = 'CloudComputing-ASU-Project1'
 
 SECURITY_GROUP_ID = 'sg-0c3a65f539fa2240a'
 
@@ -40,5 +40,5 @@ USERDATA = '''#!/bin/bash
 /usr/bin/python3 /home/ubuntu/cloud_project/cloud_iaas_project/apptier.py'''
 
 INSTANCE_PROFILE = {
-    'Arn':'arn:aws:iam::115873875546:instance-profile/EC2_FA_Role',
+    'Arn':'arn:aws:iam::115873875546:instance-profile/sample_webtier',
 }
