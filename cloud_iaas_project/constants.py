9a1db1bfbff717f81dc5ea69f997f09354aecfd9
shreyapat4 <spate100@asu.edu>
2021-03-11 19:41:59 -0700

    Updated latest AMI code

--- cloud_iaas_project/constants.py
+++ cloud_iaas_project/constants.py
@@ -15,7 +15,7 @@ KEY_NAME = 'newkey'
 SECURITY_GROUP_ID = 'sg-0c7f568aaf89e6cd9'
 
 # replace this with custom ami with app tier logic!!
-AMI_IMAGE_ID = 'ami-09ff9c1493dc72355' 
+AMI_IMAGE_ID = 'ami-0f9f2feff7e7f5e6d' 
 OG_AMI_IMAGE_ID = 'ami-0ee8cf7b8a34448a6'
 
 MIN_APP_TIERS = 0
